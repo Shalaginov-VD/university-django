5afaa665f74de44b3867656dc775864bb54d6ca8
Shalaginov-VD <145598389+Shalaginov-VD@users.noreply.github.com>
2025-02-24 15:19:40 +0500

    Add files via upload

---
+++ polls/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Stundent, Group
+
+admin.site.register(Stundent)
+admin.site.register(Group)
---
+++ polls/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.group_name
+
+class Stundent(models.Model):
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.middle_name}'
